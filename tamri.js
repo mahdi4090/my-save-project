ce097995c53f8eb081373217345aab26f16798ef
mahdi <mahdi3090mirzaei@gmail.com>
2023-08-09 11:52:16 +0330

    the new git save

---
+++ tamri.js
@@ -0,0 +1,18 @@
+// //var name='Mahdi';
+// var number=24 ;
+// var number2=24 ;
+// //alert(name+' '+family);
+// var name="mahdi";
+// //alert(name+" "+family);
+// //document.write("hello word")
+// console.log("conected...");
+// var isEqual;
+// isEqual = (number==number2);
+// document.write(isEqual);
+// console.log(isEqual);
+
+
+
+
+
+
