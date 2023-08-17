74a0ea10935c72cee84d9252d9063e5e854ad206
mahdi4090 <mahdi4090mirzaei@gmail.com>
2023-08-17 23:19:12 +0330

    the first js

--- tamri.js
+++ tamri.js
@@ -1,15 +1,38 @@
-// //var name='Mahdi';
-// var number=24 ;
-// var number2=24 ;
-// //alert(name+' '+family);
-// var name="mahdi";
-// //alert(name+" "+family);
-// //document.write("hello word")
+
+// var Name , LastName , Age;
+
+
+// Name="mahdi";
+// LastName="mirzaei";
+// Age=17;
+
+// var  a, b, c;
+
+// a= 20;
+// b=30;
+// c= a*b;
+
+
+// document.write(Age+c)
+// window.alert("hello im "+Name+' '+LastName)
 // console.log("conected...");
-// var isEqual;
-// isEqual = (number==number2);
+
+
+
+// var Number= 38;//Number
+// var Number2=3;
+// var Name= 'mahdi';//strong
+// var iscomplete= true;//boolean
+// var isEqual= (Number==Number2); //boolean
 // document.write(isEqual);
-// console.log(isEqual);
+// var person= { FerstName:"mahdi" , LastName: "mirzaei"}; //object
+// console.log(person.FerstName+" "+person.LastName);
+ 
+
+
+
+
+
 
 
 
